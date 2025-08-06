9f2736a85a517fac0ded7ae52cc85a74a3e0cb75
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2025-08-06 23:51:45 +0700

    update

---
+++ src/index.d.ts
@@ -0,0 +1,29 @@
+declare module '@/components/*' {
+  const component: any;
+  export default component;
+}
+
+declare module '@/pages/*' {
+  const page: any;
+  export default page;
+}
+
+declare module '@/store/*' {
+  const store: any;
+  export default store;
+}
+
+declare module '@/services/*' {
+  const service: any;
+  export default service;
+}
+
+declare module '@/styles/*' {
+  const styles: any;
+  export default styles;
+}
+
+declare module '@/utils/*' {
+  const util: any;
+  export default util;
+} 
